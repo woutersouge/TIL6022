61dc34e5123445eb2054123017825a2fee334ed6
Tim Traas <t.j.j.traas@student.utwente.nl>
2022-10-14 16:29:50 +0200

    All drop downs added

--- dashboard.py
+++ dashboard.py
@@ -6,9 +6,18 @@ import geopandas as gpd
 import geojson
 import numpy as np
 #Description
+#Open dataset
+dataset = 'TILL6022_Emission_Dataset.csv'
+df = pd.read_csv(dataset, delimiter=',', encoding='ISO-8859-1')
+df.date = pd.to_datetime(df.date)
+
+# Side bar code
 sidebar = st.sidebar
 sidebar.title('Carbon Emissions in Transport')
 sidebar.write('Test 123')
+
+
+#Drop down menus
 given_time = st.selectbox('Select time period to visualise', ['Day','Month','Quartile','Year'])
 if given_time == 'Day':
     date_filt = 'D'
@@ -18,6 +27,11 @@ elif given_time == 'Quartile':
     date_filt = 'QS'
 elif given_time == 'Year':
     date_filt = 'Y'
+
+given_sector = st.selectbox('Select sector to visualise', df.sector.unique())
+
+default_country = int(np.where(df['country'].unique() == 'WORLD')[0])
+given_country = st.selectbox('Select country to visualise', df.country.unique(), index = default_country)
 # """
 # This application is a simple example of dashboarding for visualising data.  
 # You can find the streamlit cheatsheet here: https://share.streamlit.io/daniellewisdl/streamlit-cheat-sheet/app.py  
@@ -38,10 +52,7 @@ elif given_time == 'Year':
 #         fig = px.bar(df_new, x=df_new.index, y="pop", color=df_new.index, range_y=[0,4000000000])
 #         st.plotly_chart(fig)    
 
-#Open dataset
-dataset = 'TILL6022_Emission_Dataset.csv'
-df = pd.read_csv(dataset, delimiter=',', encoding='ISO-8859-1')
-df.date = pd.to_datetime(df.date)
+
 df_filt = df.groupby(['country','sector']).resample(date_filt,on='date').sum()    #D for Day, MS for month, QS to quarter, Y for year
 df_filt = df_filt.reset_index()
 df_filt.date = np.datetime_as_string(df_filt.date, unit='D')
@@ -64,7 +75,7 @@ fig.update_layout(
     geo = dict(
         showframe = False,
         showcoastlines = True,
-        projection_type = "equirectangular"
+        projection_type = "equirectangular"        
     )
 )
 st.plotly_chart(fig)
