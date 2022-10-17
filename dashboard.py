fdd93a8a4202880e582ea36b05fcd15040da7eb6
Tim Traas <t.j.j.traas@student.utwente.nl>
2022-10-17 09:40:34 +0200

    Zoom function added

--- dashboard.py
+++ dashboard.py
@@ -8,8 +8,16 @@ import numpy as np
 #Description
 #Open dataset
 dataset = 'TILL6022_Emission_Dataset.csv'
-df = pd.read_csv(dataset, delimiter=',', encoding='ISO-8859-1')
+df = pd.read_csv(dataset, delimiter=',', encoding='ISO-8859-1') #,parse_dates = ['date']
+country_bb = pd.read_csv('country_bb.csv', delimiter=',', encoding='ISO-8859-1')
+# country_bb.set_index('country', inplace=True)
+#df.set_index('country', inplace=True)
+#sf = df[df.date == '2019-01-01']
+# vf = df[df.sector == 'Total']
 df.date = pd.to_datetime(df.date)
+df_filt = df.groupby(['country','sector']).resample('MS',on='date').sum()    #D for Day, MS for month, QS to quarter, Y for year
+df_filt = df_filt.reset_index()
+df_filt.date = np.datetime_as_string(df_filt.date, unit='D')
 
 # Side bar code
 sidebar = st.sidebar
@@ -32,53 +40,48 @@ given_sector = st.selectbox('Select sector to visualise', df.sector.unique())
 
 default_country = int(np.where(df['country'].unique() == 'WORLD')[0])
 given_country = st.selectbox('Select country to visualise', df.country.unique(), index = default_country)
-# """
-# This application is a simple example of dashboarding for visualising data.  
-# You can find the streamlit cheatsheet here: https://share.streamlit.io/daniellewisdl/streamlit-cheat-sheet/app.py  
-# """
-# )
-
-# #Data
-# df = px.data.gapminder()
-# years = df.year.unique()
-
-# given_year = st.selectbox('Select year to visualise', years)
-# st.write('You selected year ' + str(given_year))
-
-# if given_year is not None:
-#     df_new = df.query('year=='+str(given_year))
-#     df_new = df_new.groupby('continent').sum()
-#     if df_new is not None:
-#         fig = px.bar(df_new, x=df_new.index, y="pop", color=df_new.index, range_y=[0,4000000000])
-#         st.plotly_chart(fig)    
+if (given_country == 'WORLD' or given_country == 'ROW' or given_country == 'EU27 & UK'):
+    latrange = [-90,90]
+    lonrange = [-180,180]
+elif given_country == 'US':
+    given_country == 'United States'
+    given_country_bb = country_bb[country_bb.country == given_country]
+    latrange = [float(given_country_bb['latmin']),float(given_country_bb['latmax'])]
+    lonrange = [float(given_country_bb['lonmin']),float(given_country_bb['lonmax'])]
+else:
+    given_country_bb = country_bb[country_bb.country == given_country]
+    latrange = [float(given_country_bb['latmin']),float(given_country_bb['latmax'])]
+    lonrange = [float(given_country_bb['lonmin']),float(given_country_bb['lonmax'])]
 
 
 df_filt = df.groupby(['country','sector']).resample(date_filt,on='date').sum()    #D for Day, MS for month, QS to quarter, Y for year
 df_filt = df_filt.reset_index()
 df_filt.date = np.datetime_as_string(df_filt.date, unit='D')
 
-with open("B:\Documenten\TU_Delft\Master\Module_1\Python\Assignment\TIL6022\countries.geojson") as f:
-    counties = geojson.load(f)
-gdf = gpd.GeoDataFrame.from_features(counties["features"], crs=4326)
-mask = gdf.area >2    # Function to remove smaller countries to speed up 
-gdf_selec = gdf.loc[mask]
 
-fig = px.choropleth(df_filt[df_filt.sector =='Domestic Aviation'], locations= "country",
+
+fig = px.choropleth(df_filt[df_filt.sector =='Domestic Aviation'], 
+                    # geojson= counties,
+                    # featureidkey='ISO_A3',
+                    locations= 'country',
                     locationmode= "country names",
-                    color="co2",
+                    color='co2',
                     #hover_name="country",
-                    animation_frame="date"
-                    #color_continuous_scale="Blues_r"
-                    )
-fig.update_geos(fitbounds=given_country)     
+                    animation_frame= 'date'
+                    #zoom=2,
+)
+fig.update_geos(lataxis_range=latrange, lonaxis_range=lonrange)  
 fig.update_layout(
-    title_text = "Life Expectancy",
+    title_text = "CO2 emmissions transport sector",
     geo = dict(
         showframe = False,
         showcoastlines = True,
-        projection_type = "equirectangular"        
+        projection_type = "equirectangular",
     ),
-    height=300, 
-    margin={"r":0,"t":0,"l":0,"b":0}
+    height=1000,
+    width  = 1000
+    
+
 )
+
 st.plotly_chart(fig)
